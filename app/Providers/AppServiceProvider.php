28bcd4c92322f2b8dfe49b4b4b11dd1672b06af6
hrithik220 <hrithik.dev.burman@g.bracu.ac.bd>
2026-04-21 11:26:04 +0600

    fix https warning issue

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -6,13 +6,21 @@ use Illuminate\Support\ServiceProvider;
 use App\Models\Resource;
 use App\Policies\ResourcePolicy;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 
 class AppServiceProvider extends ServiceProvider
 {
-    public function register(): void {}
+    public function register(): void
+    {
+        //
+    }
 
     public function boot(): void
     {
         Gate::policy(Resource::class, ResourcePolicy::class);
+
+        if (env('APP_ENV') === 'production') {
+            URL::forceScheme('https');
+        }
     }
 }
